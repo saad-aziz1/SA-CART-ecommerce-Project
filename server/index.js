a8ef189e6300e1834d3456289b211ea5c2d13759
saad-aziz1 <saadaziz10000@gmail.com>
2026-02-15 12:29:43 +0500

    Fixed CORS for production

--- server/index.js
+++ server/index.js
@@ -11,20 +11,24 @@ import cartRouter from './Routers/cartRouter.js';
 import orderRouter from './Routers/orderRoutes.js'
 
 const app = express()
+
+// Updated CORS for Production and Local development
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
+    origin: ["http://localhost:5173", "*"], 
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
 }))
+
 app.use(express.json({ limit: "50mb" })); 
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
-app.use(express.urlencoded({ extended: true }))
+// Pehle se maujood urlencoded ko limit ke sath merge kar diya hy
 app.use(cookieParser())
+
 app.use('/api/user', userRouter)
 app.use('/api/product',productRouter)
 app.use('/api/cart', cartRouter);
 app.use('/api/order', orderRouter);
-
-
 
 const port = process.env.PORT || 5000
 app.listen(port,()=>{
