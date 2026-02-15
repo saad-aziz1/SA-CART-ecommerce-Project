2916545dbb6ca21333457aa716fd26bc7b724fb1
saad-aziz1 <saadaziz10000@gmail.com>
2026-02-15 16:37:53 +0500

    fix: update CORS origin to allow Vercel production domain

--- server/index.js
+++ server/index.js
@@ -14,7 +14,10 @@ const app = express()
 
 // Updated CORS for Production and Local development
 app.use(cors({
-    origin: ["http://localhost:5173", "*"], 
+    origin: [
+        "http://localhost:5173", 
+        "https://sa-cart-ecommerce-project.vercel.app"
+    ], 
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"]
@@ -22,7 +25,6 @@ app.use(cors({
 
 app.use(express.json({ limit: "50mb" })); 
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
-// Pehle se maujood urlencoded ko limit ke sath merge kar diya hy
 app.use(cookieParser())
 
 app.use('/api/user', userRouter)
