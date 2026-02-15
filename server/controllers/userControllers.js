27472a905237c31751e29908811f2e67fa61fe40
saad-aziz1 <saadaziz10000@gmail.com>
2026-02-15 21:08:44 +0500

    Fix production auth cookie settings (secure + sameSite none)

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -137,11 +137,12 @@ export const logIn = async (req, res) => {
         const token = generateToken(user._id)
 
         res.cookie('token', token, {
-            httpOnly: true,
-            secure: false,
-            sameSite: "lax",
-            maxAge: 7 * 24 * 60 * 60 * 1000
-        })
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    maxAge: 7 * 24 * 60 * 60 * 1000
+});
+
 
 
 
