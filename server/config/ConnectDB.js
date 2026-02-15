ea6ddbe6277d2fd7e83b733a2b21a7dd17088fd9
saad-aziz1 <saadaziz10000@gmail.com>
2026-02-15 11:26:19 +0500

    Connected to MongoDB Atlas and updated gitignore

--- server/config/ConnectDB.js
+++ server/config/ConnectDB.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const dbConnect = async () => {
    try {
     await mongoose.connect(process.env.MONGODB_URL)
-    console.log("DB Connected at local");
+    console.log("DB Connected at Cloud");
     
    } catch (error) {
     console.log("DB Connection Error",error);
