a447cb03828825b7c77b95cb0fcd9f5318f3a8bb
saad-aziz1 <saadaziz10000@gmail.com>
2026-02-16 12:15:46 +0500

    fix: change SMTP port to 465 and enable SSL to fix timeout error

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -2,31 +2,33 @@ import nodemailer from "nodemailer";
 
 const sendEmail = async ({ to, subject, html }) => {
   try {
-    
     const transporter = nodemailer.createTransport({
       host: "smtp-relay.brevo.com",
-      port: 587,
-      secure: false, 
+      // Port change
+      port: 465, 
+      secure: true, 
       auth: {
-        user: process.env.BREVO_USER, 
-        pass: process.env.BREVO_API_KEY, 
+        user: process.env.BREVO_USER,
+        pass: process.env.BREVO_API_KEY,
       },
+      
+      connectionTimeout: 10000, 
     });
 
     const mailOptions = {
-      from: `"SA-Cart" <${process.env.BREVO_USER}>`, 
-      to: to,
-      subject: subject,
-      html: html,
+      from: `"SA-Cart" <${process.env.BREVO_USER}>`,
+      to,
+      subject,
+      html,
     };
 
-    
     const info = await transporter.sendMail(mailOptions);
-    console.log("Email sent successfully via Brevo:", info.messageId);
+    console.log("Email Result:", info.response);
     return info;
 
   } catch (error) {
-    console.error("Brevo SMTP Error:", error);
+    
+    console.error("Brevo Connection Error:", error);
     throw error;
   }
 };
