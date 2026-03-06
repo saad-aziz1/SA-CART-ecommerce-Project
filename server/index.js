c68573916362216f061af72562377525a221d785
saad-aziz1 <saadaziz10000@gmail.com>
2026-03-06 10:35:08 +0500

    add root route for uptime monitoring

--- server/index.js
+++ server/index.js
@@ -27,6 +27,10 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser())
 
+app.get("/", (req, res) => {
+  res.send("Ecommerce API running");
+});
+
 app.use('/api/user', userRouter)
 app.use('/api/product',productRouter)
 app.use('/api/cart', cartRouter);
